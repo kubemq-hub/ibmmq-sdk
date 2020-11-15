6081f1025c1294d2cf4113a3c538dd6cda78ae96
kubemq <lior.nabat@kubemq.io>
2020-11-15 18:09:29 +0200

    add place holder for win

--- mq-golang/mqi.go
+++ mq-golang/mqi.go
@@ -46,8 +46,8 @@ package mq_golang
 #cgo  windows      CFLAGS:  -I"C:/Program Files/IBM/MQ/Tools/c/include"
 #cgo !windows,!aix,!darwin LDFLAGS: -L/opt/mqm/lib64 -lmqm_r -Wl,-rpath,/opt/mqm/lib64 -Wl,-rpath,/usr/lib64
 #cgo darwin                LDFLAGS: -L/opt/mqm/lib64 -lmqm_r -Wl,-rpath,/opt/mqm/lib64 -Wl,-rpath,/usr/lib64
-#cgo aix                   LDFLAGS: -L/usr/mqm/lib64 -lmqm_r 
-#cgo windows               LDFLAGS: -L "C:/Program Files/IBM/MQ/bin64" -lmqm
+#cgo aix                   LDFLAGS: -L/usr/mqm/lib64 -lmqm_r
+#cgo windows               LDFLAGS: -L "./bin64" -lmqm
 
 #include <stdlib.h>
 #include <string.h>
